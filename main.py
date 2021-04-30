6d267f17143e1f398011c0f85c937db4316d0005
lispalding <lisette.spalding@gmail.com>
2021-04-30 12:48:14 -0600

    04/30/2021 Commit -- 12:48 PM

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # FILE NAME: main.py
 # PROJECT NAME: machine_learning_experiment__pytorch
 # DATE CREATED: 04/29/2021
-# DATE LAST MODIFIED: 04/29/2021
+# DATE LAST MODIFIED: 04/30/2021
 # PYTHON VER. USED: 3.x
 
 ############################## IMPORTS ##############################
@@ -31,6 +31,7 @@ data = pd.read_csv(dataSet, sep = ";") # Since our data is separated with semico
 print(data.head())
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]] # Collecting only the relevant data from the data set
+data = shuffle(data) # Optional -- Shuffling the data
 ## Data collection and loading FIN
 
 ## Separating the data:
@@ -40,24 +41,68 @@ x = np.array(data.drop([predict], 1)) # Features
 y = np.array(data[predict]) # Labels
 ## Separating the data FIN
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+xTrain, xTest, yTrain, yTest = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 ################################ FIN ################################
 
 ############################# ALGORITHM #############################
-linear = linear_model.LinearRegression() # Defining the linear model that will be used
 
-linear.fit(x_train, y_train)
-accuracy = linear.score(x_test, y_test)
+################ TESTING CODE ################
+# linear = linear_model.LinearRegression() # Defining the linear model that will be used
+#
+# linear.fit(xTrain, yTrain)
+# accuracy = linear.score(x_test, y_test)
+#
+# print(accuracy) # Checking how well the algorithm preformed on the test
+#
+# ## Viewing Constants:
+# print("Coefficient: \n", linear.coef_) # These are each slope value
+# print("Intercept: \n", linear.intercept_) # This is the intercept
+# ## Constants FIN
+#
+# predictions = linear.predict(xTest) # Gets a list of all predictions
+#
+# for x in range(len(predictions)):
+#     print(predictions[x], xTest[x], yTest[x])
+################ TESTING FIN #################
 
-print(accuracy) # Checking how well the algorithm preformed on the test
+## Training the model multiple times for best possible score
+best = 0
+for _ in range(20):
+    xTrain, xTest, yTrain, yTest = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-## Viewing Constants:
-print("Coefficient: \n", linear.coef_) # These are each slope value
-print("Intercept: \n", linear.intercept_) # This is the intercept
-## Constants FIN
+    linear = linear_model.LinearRegression() # Defining the linear model taht will be used
 
-predictions = linear.predict(x_test) # Gets a list of all predictions
+    linear.fit(xTrain, yTrain)
+    accuracy = linear.score(xTest, yTest)
 
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+    print("Accuracy: " + str(accuracy))
+
+    if accuracy > best:
+        best = accuracy
+        with open("studentgrades.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+## Loading Model
+pickleIn = open("studentgrades.pickle", "rb")
+linear = pickle.load(pickleIn)
+
+print("-------------------------")
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
+print("-------------------------")
+
+predicted = linear.predict(xTest)
+
+for x in range(len(predicted)):
+    print(predicted[x], xTest[x], yTest[x])
+
+## Drawing and plotting the model
+plot = "failures"
+
+plt.scatter(data[plot], data["G3"])
+plt.legend(loc = 4)
+plt.xlabel(plot)
+plt.ylabel("Final Grade")
+
+plt.show()
 ################################ FIN ################################
